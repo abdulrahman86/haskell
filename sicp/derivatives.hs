00a24c5ece1108a9a300cf2aeccdead497ce3b37
Abdul Sattar <asattar@exchangesolutions.com>
2016-03-08 07:20:04 -0500

    small modification to derivative

--- sicp/derivatives.hs
+++ sicp/derivatives.hs
@@ -25,7 +25,7 @@ derivative (Var y) (Var x)
                             | y == x  = Const 1
                             | otherwise = Const 0
 
-derivative (x :+: y) (Var z) = simplify (simplify((derivative x (Var z))) :+: simplify((derivative y (Var z))))
+derivative (x :+: y) (Var z) = simplify ((derivative x (Var z)) :+: (derivative y (Var z)))
 derivative (x :*: y) (Var z) = simplify(simplify((x :*: (derivative y (Var z)))) :+: simplify((y :*: (derivative x (Var z)))))
 
 
